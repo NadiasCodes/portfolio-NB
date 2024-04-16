38378e1f71c5b1a092398198421ecc1540108bfc
NadiasCodes <De.nadia@outlook.com>
2024-04-16 13:24:15 +0200

    configuring my hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,38 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "./Hero.css";
+import { Container, Row, Col } from "react-bootstrap";
 import Typewriter from "typewriter-effect";
-
-import heroImage from "../../assets/hero-images/nadia2.png";
+import { BsArrowRightCircle } from "react-icons/bs";
+import coder from "../../assets/hero-images/nadia2.png"
+import "./Hero.css";
 
 
 const Hero = () => {
   return (
-   
-     <div>hello</div>
-   
+    <section className="hero" id="home">
+      <Container>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <span className="tagline">Welcome to my Portfolio</span>
+            <h1>
+              Hi I'm Nadia a
+              <Typewriter
+                onInit={(typewriter) => {
+                  typewriter.typeString("Front-End developer").pauseFor(100).deleteAll().typeString("React developer").start();
+                }}
+              />
+            </h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, obcaecati nemo soluta quidem error possimus cum? Praesentium atque, asperiores harum id tempora sunt ullam quasi cupiditate nostrum assumenda maiores distinctio.</p>
+
+            <button>
+              Contact Me <BsArrowRightCircle size={25} />
+            </button>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={coder} alt="hero image" />
+          </Col>
+        </Row>
+      </Container>
+    </section>
   );
 };
 
