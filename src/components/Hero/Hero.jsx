94c37c2f517e34585c6765b4a5923b33231b8379
NadiasCodes <De.nadia@outlook.com>
2024-04-16 16:34:52 +0200

    added aos library for animations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,13 +9,16 @@ import LinkedinIcon from "../../assets/icons/linkedin.png";
 import "./Hero.css";
 
 const Hero = () => {
+  AOS.init();
   return (
     <section className="hero" id="home">
       <Container>
         <Row className="align-items-center">
           <Col xs={12} md={6} xl={7}>
-            <span className="tagline">Welcome to my Portfolio</span>
-            <h1>
+            <span className="tagline" data-aos="fade-down" data-aos-easing="linear" data-aos-duration="1500">
+              Welcome to my Portfolio
+            </span>
+            <h1 data-aos="fade-right" data-aos-duration="1500">
               Hi, I'm Nadia a
               <Typewriter
                 onInit={(typewriter) => {
@@ -23,9 +26,11 @@ const Hero = () => {
                 }}
               />
             </h1>
-            <p>Junior front-end developer, proficient in React, with a keen interest in expanding skills in Tailwind CSS, Next.js, and Framer Motion. Dedicated to creating visually appealing, user-friendly web experiences through clean, organized code and a passion for continuous learning."</p>
+            <p data-aos="fade-right" data-aos-duration="1200" data-aos-offset="300" data-aos-easing="ease-in-sine">
+              Junior front-end developer, proficient in React, with a keen interest in expanding skills in Tailwind CSS, Next.js, and Framer Motion. Dedicated to creating visually appealing, user-friendly web experiences through clean, organized code and a passion for continuous learning.
+            </p>
 
-            <div className="social-icons-container">
+            <div className="social-icons-container" data-aos="flip-left" data-aos-easing="ease-out-cubic" data-aos-duration="2000">
               <a href="#">
                 <img src={GitHubIcon} alt="" className="social-icon" />
               </a>
@@ -38,17 +43,12 @@ const Hero = () => {
                 <img src={DevIcon} alt="" className="social-icon" />
               </a>
             </div>
-            <button className="main-button">
+            <button className="btn-hero " data-aos="zoom-in-up" data-aos-duration="900">
               Contact Me <BsArrowRightCircle size={25} className="arrow" />
             </button>
-            <div className="button-container">
-              <button className="main-button">
-                My Projects <BsArrowRightCircle size={25} className="arrow" />
-              </button>
-            </div>
           </Col>
           <Col xs={12} md={6} xl={5}>
-            <img src={coder} alt="hero image" />
+            <img src={coder} alt="hero image" className="coder" />
           </Col>
         </Row>
       </Container>
