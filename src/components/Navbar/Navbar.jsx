fdfdec1426ea30ef083e4514ce7a473d90fc2dbd
NadiasCodes <De.nadia@outlook.com>
2024-04-16 10:15:18 +0200

    added media queries and a onClick function for the navbar menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-import logo from "../../assets/hero-images/logo.png"
+import menuIcon from "../../assets/icons/menu-icon.png"
 
 const Navbar = () => {
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+}
+
   return (
     <nav className="nav-container">
       <h1>NB</h1>
-      <ul>
+      <ul className={mobileMenu ? "" : "hide-mobile-menu"}>
         <li>Home</li>
         <li>About</li>
         <li>Projects</li>
         <li>Contact</li>
       </ul>
+      <img src={menuIcon} alt="menu icon" className="menu-icon" onClick={toggleMenu} />
     </nav>
   );
 };
