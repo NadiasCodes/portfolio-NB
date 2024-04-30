4bab63628f00a7c0b2a467ef3eaf09d70b747fb2
NadiasCodes <De.nadia@outlook.com>
2024-04-30 09:48:09 +0200

    added i18 nexus package for multi-traslations

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App.jsx";
 import AOS from "aos";
-
 import "aos/dist/aos.css";
 import "./index.css";
+import i18next from "i18next";
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
